f40ecc89bb9c40725ef74344f38a1fdcdae86fd6
harryd13 <35573650+harryd13@users.noreply.github.com>
2020-05-23 03:51:10 +0530

    Update UnitTest1.cs

--- dummytest1/UnitTest1.cs
+++ dummytest1/UnitTest1.cs
@@ -7,15 +7,18 @@ namespace seleniumlrn
     [TestFixture]
     public class Tests
     {
-
+        //instantiation of webdriver
         IWebDriver driver;
 
         [OneTimeSetUp]
         public void Setup()
-        {
+        {   
+            //to use chrome for testing
             driver = new ChromeDriver();
         }
-
+        
+        
+        //this will generate multiple testcases (code reuse)
         [TestCase(4)]
         [TestCase(5)]
         [TestCase(45)]
@@ -23,13 +26,16 @@ namespace seleniumlrn
         [TestCase(6784)]
         public void TestAgeRange(int age)
         {
+            //give the path of your html page
             driver.Url = "file:///home/harman/Desktop/seleniumlrn/web/page.html";
             IWebElement txtAge = driver.FindElement(By.Id("txtAge"));
             IWebElement validations = driver.FindElement(By.Id("validations"));
-
+            
+            //convert age to a string as Sendkeys() accepts a string val.
             txtAge.SendKeys(age.ToString());
             txtAge.SendKeys("\t");
-
+            
+            //main test
             string validationText = validations.GetAttribute("innerHTML");
             if (validationText == "")
             {
@@ -44,6 +50,7 @@ namespace seleniumlrn
         [OneTimeTearDown]
         public void close()
         {
+            //close the chrome tab
             driver.Close();
         }
     }
